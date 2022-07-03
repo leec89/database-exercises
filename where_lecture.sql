d140c5d5cd102e4a949303a2bb9606420106a2cb
leec89 <leec89@gmail.com>
2022-07-03 11:44:35 -0500

    feat: messin with mysql join

--- where_lecture.sql
+++ where_lecture.sql
@@ -48,6 +48,8 @@ SELECT * FROM salaries ORDER BY salary DESC LIMIT 5;
 
 SELECT * FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
 
+# FUNCTIONS EXERCISE
+
 SELECT WEEKDAY(NOW());
 
 SELECT first_name from employees order by first_name;   # 300024 results
@@ -55,4 +57,74 @@ SELECT first_name from employees order by first_name;   # 300024 results
 SELECT first_name from employees group by first_name;   # 1275 results
 
 SELECT DISTINCT first_name from employees order by first_name;  # 1275 results
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees  WHERE (last_name LIKE 'E%') OR (last_name LIKE '%E');
+
+SELECT * FROM employees limit 25;
+
+SELECT * FROM employees WHERE month(birth_date) = 12
+                          AND day(birth_date) = 25;
+
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999
+                            AND month(birth_date) = 12
+                            AND day(birth_date) = 25;
+
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999
+                          AND birth_date LIKE '%12-25%';
+
+
+SELECT *, DATEDIFF(hire_date, birth_date) from employees WHERE year(hire_date) BETWEEN 1990 AND 1999
+                                                        AND birth_date LIKE '%12-25%';
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, DATEDIFF(curdate(), hire_date) AS number_days from employees WHERE year(hire_date) BETWEEN 1990 AND 1999
+                                                           AND birth_date LIKE '%12-25%' ORDER BY number_days DESC;
+
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999
+                          AND birth_date LIKE '%12-25%' ORDER BY hire_date, birth_date;
+
+SELECT *, DATEDIFF(CURDATE(), hire_date)
+FROM employees
+WHERE year (hire_date) BETWEEN 1990 AND 2000 AND birth_date LIKE '%-12-25%';
+
+# GROUP BY EXERCISE
+
+SELECT DISTINCT title FROM titles;
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees  WHERE (last_name LIKE 'E%') OR (last_name LIKE '%E') GROUP BY last_name;
+
+SELECT last_name
+FROM employees
+WHERE (last_name LIKE 'E%') AND (last_name LIKE '%E')
+GROUP BY last_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE (last_name LIKE 'E%') AND (last_name LIKE '%E')
+GROUP BY first_name, last_name;
+
+SELECT DISTINCT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%E' AND last_name LIKE 'E%'
+GROUP BY first_name, last_name
+ORDER BY first_name, last_name;
+
+SELECT COUNT(*), last_name FROM employees
+        WHERE (last_name LIKE '%q%') AND (last_name NOT LIKE '%qu%')
+        GROUP BY last_name;   # 547 results
+
+SELECT COUNT(*), gender FROM employees
+         WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+         GROUP BY gender;
+
+# ALIAS EXERCISE
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees LIMIT 10;
+
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB FROM employees LIMIT 10;
+
+SELECT e.emp_no, CONCAT(e.first_name, ' ', e.last_name) AS full_name, s.salary, e.hire_date
+    FROM employees e LEFT JOIN salaries s
+    ON e.emp_no = s.emp_no
+    WHERE s.to_date LIKE '9999%' order by salary DESC;
+
 
