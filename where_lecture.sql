112764b936dc571f8a57f559d8943b7c6692f8cb
leec89 <leec89@gmail.com>
2022-07-06 12:19:55 -0500

    feat: whoops, forgot to push

--- where_lecture.sql
+++ where_lecture.sql
@@ -1,4 +1,7 @@
 use employees;
+use departments;
+use dept_emp;
+use title;
 
 # WHERE EXERCISE
 
@@ -128,3 +131,134 @@ SELECT e.emp_no, CONCAT(e.first_name, ' ', e.last_name) AS full_name, s.salary, 
     WHERE s.to_date LIKE '9999%' order by salary DESC;
 
 
+DESCRIBE employees;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE salaries;
+DESCRIBE titles;
+
+SELECT * from employees;
+SELECT * from departments;
+SELECT * from dept_emp;
+SELECT * from dept_manager;
+SELECT * from salaries;
+SELECT * from titles;
+
+# JOINS - Employees Database exercises
+# (2) Find department along with the name of the current manager for that department.
+
+SELECT d.dept_name AS 'Department Name', concat(e.first_name, ' ', e.last_name) AS 'Department Manager'
+    FROM employees e
+        JOIN dept_manager dm ON dm.emp_no = e.emp_no
+        JOIN departments d ON d.dept_no = dm.dept_no
+    WHERE dm.to_date LIKE '9999%'
+    ORDER BY d.dept_name;
+
+# (1) Find the name of all departments currently managed by women.
+
+SELECT d.dept_name AS 'Department Name', concat(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees e
+         JOIN dept_manager dm ON dm.emp_no = e.emp_no
+         JOIN departments d ON d.dept_no = dm.dept_no
+WHERE dm.to_date LIKE '9999%' AND e.gender = 'f'
+ORDER BY d.dept_name;
+
+# (1) Find the current titles of employees currently working in the Customer Service department.
+
+SELECT t.title AS 'title', count(*) AS 'Total'
+    FROM employees e
+        JOIN titles t on e.emp_no = t.emp_no
+        JOIN dept_emp de on e.emp_no = de.emp_no
+        JOIN departments d on de.dept_no = d.dept_no
+    WHERE d.dept_name = 'Customer Service' AND t.to_date > NOW() AND de.to_date > NOW()
+    group by t.title
+    order by count(*) DESC;
+
+# (1) Find the current salary of all current managers.
+
+SELECT dm.to_date, d.dept_name AS 'Department Name', concat(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+    FROM employees e
+        JOIN salaries s ON e.emp_no = s.emp_no
+        JOIN dept_manager dm ON dm.emp_no = e.emp_no
+        JOIN departments d ON d.dept_no = dm.dept_no
+    WHERE s.to_date > NOW() AND dm.to_date > NOW()
+    ORDER BY d.dept_name;
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee', d.dept_name AS 'Department', CONCAT(e.first_name, ' ', e.last_name) AS 'Manager'
+    FROM employees e
+        LEFT JOIN departments d on e.emp_no = d.dept_name;
+
+# SUB-QUERY EXERCISE
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+)
+LIMIT 10;
+
+SELECT * FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees e
+    WHERE e.emp_no = 101010
+);
+
+SELECT * FROM employees
+WHERE birth_date = '1957-08-11'
+
+SELECT t.title AS 'title', count(*) AS 'Total'
+FROM employees e
+JOIN titles t on e.emp_no = t.emp_no
+WHERE first_name IN (
+    SELECT first_name
+    FROM employees
+    WHERE first_name = 'Aamod'
+    )
+group by t.title
+order by count(*) DESC;
+
+SELECT count(*)
+FROM titles t
+WHERE t.emp_no IN (
+    SELECT e.emp_no
+    FROM employees e
+    WHERE first_name = 'Aamod'
+);
+
+SELECT *
+FROM employees
+WHERE first_name = 'Aamod';
+
+
+SELECT first_name, last_name
+from employees e
+where e.gender = 'F' AND e.emp_no IN (
+    SELECT dm.emp_no
+    FROM dept_manager dm
+    WHERE to_date > NOW()
+);
+
+SELECT d.dept_name
+    FROM departments d
+    JOIN dept_emp de on d.dept_no = de.dept_no
+    JOIN employees e on de.emp_no = e.emp_no
+    JOIN dept_manager dm on e.emp_no = dm.emp_no
+    where e.gender = 'F' AND dm.to_date > NOW()
+group by d.dept_name;
+
+
+SELECT d.dept_name
+FROM departments d
+WHERE d.dept_no IN (
+    SELECT dm.dept_no
+    FROM dept_manager dm
+    WHERE dm.to_date > NOW() AND dm.emp_no IN (
+            SELECT e.emp_no
+            FROM employees e
+            WHERE e.gender = 'F'
+    )
+)
+group by d.dept_name;
